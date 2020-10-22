6b678602026856d87994ffba4d2695a617e96248
Will <williamconquergood@gmail.com>
2020-10-21 20:28:23 -0400

    Started java basics

--- src/main/java/teaching/programming/Primary.java
+++ src/main/java/teaching/programming/Primary.java
@@ -3,9 +3,44 @@ package teaching.programming;
 
 public class Primary{
 
+	static int variable = 10;
+
+
+	static int pineapples = 5;
+
+	static String text = "Really pineapples";
+
+	static String text2 = "Pineapples Are Awesome";
+
+	static boolean up = true;
+	static boolean down = false;
+
+	static int biggestInt = 2147483647;
+	static double biggestDouble = 1.7e+308;
+
+	
 
 
 	public static void main(String[] args) {
+	
+		
+
+	
+		// This print all even numbers!
+		/**
+		 * this is
+		 * a muliline comment
+		 */
+		for(int i = 0; i < 100; i+=1){
+			if(i > 50){
+				System.out.println(i);
+			}else{
+				System.out.println("This isn't big enough");
+			}
+		}
+
+		
+
 		
 	}
 
